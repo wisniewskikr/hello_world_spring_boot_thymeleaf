5a94c102a846fd0a483c9ca477c6feef3a77c122
Wisniewski <wisniewk@emea.roche.com>
2017-05-11 10:11:09 +0200

    Improve input controller

--- src/main/java/pl/kwi/springboot/controllers/InputController.java
+++ src/main/java/pl/kwi/springboot/controllers/InputController.java
@@ -1,21 +1,14 @@
 package pl.kwi.springboot.controllers;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/input")
 public class InputController {
 
-	// inject via application.properties
-	@Value("${welcome.message:test}")
-	private String message = "Hello World";
-
-	@RequestMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
+	@RequestMapping
+	public String displayPage() {
 		return "input";
 	}
 
