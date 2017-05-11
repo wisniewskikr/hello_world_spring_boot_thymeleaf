ef857e981fca406388d19a6944b3d7df91566d5d
Wisniewski <wisniewk@emea.roche.com>
2017-05-11 10:20:41 +0200

    Improve input to handle form

--- src/main/java/pl/kwi/springboot/controllers/InputController.java
+++ src/main/java/pl/kwi/springboot/controllers/InputController.java
@@ -1,7 +1,13 @@
 package pl.kwi.springboot.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pl.kwi.springboot.commands.InputCommand;
 
 @Controller
 @RequestMapping(value="/input")
@@ -9,6 +15,13 @@ public class InputController {
 
 	@RequestMapping
 	public String displayPage() {
+		return "input";
+	}
+	
+	@RequestMapping(value="/handle-button-ok", method=RequestMethod.POST)
+	public String handleButtonOk(
+			@Valid @ModelAttribute("command")InputCommand command) {
+		System.out.println("name: " + command.getName());
 		return "input";
 	}
 
