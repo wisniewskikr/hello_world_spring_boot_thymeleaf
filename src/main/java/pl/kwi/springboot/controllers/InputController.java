484c3305d82cb68b1694405c4a88d6792d758d0b
Wisniewski <wisniewk@emea.roche.com>
2017-05-11 10:25:29 +0200

    Add NameService

--- src/main/java/pl/kwi/springboot/controllers/InputController.java
+++ src/main/java/pl/kwi/springboot/controllers/InputController.java
@@ -2,16 +2,21 @@ package pl.kwi.springboot.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.kwi.springboot.commands.InputCommand;
+import pl.kwi.springboot.services.NameService;
 
 @Controller
 @RequestMapping(value="/input")
 public class InputController {
+	
+	@Autowired
+	private NameService nameService;
 
 	@RequestMapping
 	public String displayPage() {
@@ -21,7 +26,7 @@ public class InputController {
 	@RequestMapping(value="/handle-button-ok", method=RequestMethod.POST)
 	public String handleButtonOk(
 			@Valid @ModelAttribute("command")InputCommand command) {
-		System.out.println("name: " + command.getName());
+		nameService.save(command.getName());
 		return "input";
 	}
 
