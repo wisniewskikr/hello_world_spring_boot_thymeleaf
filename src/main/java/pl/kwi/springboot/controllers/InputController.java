f96bf15c6c810c03665d39b4381cfd78e96e321c
Wisniewski <wisniewk@emea.roche.com>
2017-05-16 08:00:39 +0200

    Add init validation

--- src/main/java/pl/kwi/springboot/controllers/InputController.java
+++ src/main/java/pl/kwi/springboot/controllers/InputController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,13 +21,21 @@ public class InputController {
 	private NameService nameService;
 
 	@RequestMapping
-	public String displayPage() {
+	public String displayPage(Model model) {
+		InputCommand command = new InputCommand();
+		model.addAttribute("command", command);
 		return "input";
 	}
 	
 	@RequestMapping(value="/handle-button-ok", method=RequestMethod.POST)
 	public String handleButtonOk(
-			@Valid @ModelAttribute("command")InputCommand command) {
+			@Valid @ModelAttribute("command")InputCommand command, 
+			BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return "input";
+		}
+		
 		nameService.save(command.getName());
 		return "redirect:/output";
 	}
