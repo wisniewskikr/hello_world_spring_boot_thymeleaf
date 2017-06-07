50fea39367a817d364d2a68e9a3c15bc07afdc5b
Wisniewski <wisniewk@emea.roche.com>
2017-06-07 09:17:18 +0200

    Init Spring Security with MySql db

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-Spring Boot + Thymeleaf + Spring Security
+Spring Boot + Thymeleaf + Spring Security + MySql
 
-Example application displays how Spring Security works. Users are stored in memory.
+Example application displays how Spring Security works. Users are stored in MySql database.
 
 Differences vs basic:
 - add SpringSecurityConfig;
 - add LoginController;
 - add security/login.html template;
 - add logout section to all secured pages;
-- add styles for security elements.
+- add styles for security elements;
+- add database dependencies in pom;
+- add datasource properties in application.properties;
+- create MySql schema in database;
+- add queries from queries.sql into MySql database.
--- src/main/java/pl/kwi/springboot/security/SpringSecurityConfig.java
+++ src/main/java/pl/kwi/springboot/security/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package pl.kwi.springboot.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,28 +10,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.
+			jdbcAuthentication()
+				.usersByUsernameQuery("select username, password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?")
+				.dataSource(dataSource);
+	}
+	
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
                 .authorizeRequests()
 					.antMatchers("/").permitAll()
-					.antMatchers("/input", "/output").hasAnyRole("USER")
+					.antMatchers("/input", "/output").access("hasRole('ROLE_ADMIN')")
 					.anyRequest().authenticated()
-                .and()
+					.and()
                 .formLogin()
 					.loginPage("/login")
+					.usernameParameter("username")
+					.passwordParameter("password")
 					.permitAll()
 					.and()
                 .logout()
 					.permitAll();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }
 }
