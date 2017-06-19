c4be9042bff2123b5ab94625023f1a6dacb8124f
Wisniewski <wisniewk@emea.roche.com>
2017-06-19 08:56:01 +0200

    Init upload txt file

--- src/main/java/pl/kwi/springboot/controllers/InputController.java
+++ src/main/java/pl/kwi/springboot/controllers/InputController.java
@@ -1,14 +1,14 @@
 package pl.kwi.springboot.controllers;
 
-import javax.validation.Valid;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import pl.kwi.springboot.commands.InputCommand;
 import pl.kwi.springboot.services.NameService;
 
 @Controller
@@ -24,10 +24,12 @@ public class InputController {
 	}
 	
 	@RequestMapping(value="/handle-button-ok", method=RequestMethod.POST)
-	public String handleButtonOk(
-			@Valid @ModelAttribute("command")InputCommand command) {
-		nameService.save(command.getName());
+	public String handleButtonOk(@RequestParam("file") MultipartFile file) throws IOException {
+		
+		String name = new String(file.getBytes());
+		nameService.save(name);
 		return "redirect:/output";
+		
 	}
 
 }
