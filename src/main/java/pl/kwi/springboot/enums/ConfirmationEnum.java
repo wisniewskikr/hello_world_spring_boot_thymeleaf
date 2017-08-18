0e077c4bff8b9cb849e41d81ed0e120ebabc9ac6
Wisniewski <wisniewk@emea.roche.com>
2017-08-18 08:32:31 +0200

    Improve registration message

--- src/main/java/pl/kwi/springboot/enums/ConfirmationEnum.java
+++ src/main/java/pl/kwi/springboot/enums/ConfirmationEnum.java
@@ -3,7 +3,7 @@ package pl.kwi.springboot.enums;
 public enum ConfirmationEnum {
 	
 	
-	REGISTRATION("You have been registered successfully."), 
+	REGISTRATION("Email with your registration request was sent to administrator. Please wait for his approval."), 
 	RESET_EMAIL("Email with reset password link was sent on your email account."),
 	RESET_CHANGE("Your password has been changed."),
 	NOT_VALID_TOKEN("Token is not valid.");
