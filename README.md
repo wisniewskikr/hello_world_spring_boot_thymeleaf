2402be15720c2b8cb839cfe6da278ef73433510b
Wisniewski <wisniewk@emea.roche.com>
2017-09-06 09:09:25 +0200

    Init Keycloak with Spring Security

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-Spring Boot + Thymeleaf + Spring Security
+Spring Boot + Thymeleaf + Spring Security + Keycloak
 
-Example application displays how Spring Security works. Users are stored in memory.
-
-Differences vs basic:
-- add SpringSecurityConfig;
-- add LoginController;
-- add security/login.html template;
-- add logout section to all secured pages;
-- add styles for security elements.
+It does not work.
---
+++ src/main/java/pl/kwi/springboot/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package pl.kwi.springboot.security;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    	KeycloakAuthenticationProvider provider = new KeycloakAuthenticationProvider();
+    	provider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(provider);
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+    
+    @Bean
+    public KeycloakConfigResolver getKeycloakConfigResolver() {
+    	return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/*").hasRole("demo-role")
+                .anyRequest().permitAll();
+    }
+}
