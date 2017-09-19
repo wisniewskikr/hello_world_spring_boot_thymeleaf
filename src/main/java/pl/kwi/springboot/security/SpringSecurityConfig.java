4b6d96e246b696caf52f3351f9b53a6b3a6a9f2b
Wisniewski <wisniewk@emea.roche.com>
2017-09-19 08:43:32 +0200

    Add metadata

--- src/main/java/pl/kwi/springboot/security/SpringSecurityConfig.java
+++ src/main/java/pl/kwi/springboot/security/SpringSecurityConfig.java
@@ -1,35 +1,53 @@
 package pl.kwi.springboot.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${security.saml2.metadata-url}")
+    String metadataUrl;
+
+    @Value("${server.ssl.key-alias}")
+    String keyAlias;
+
+    @Value("${server.ssl.key-store-password}")
+    String password;
+
+    @Value("${server.port}")
+    String port;
+
+    @Value("${server.ssl.key-store}")
+    String keyStoreFilePath;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable()
-                .authorizeRequests()
-					.antMatchers("/").permitAll()
-					.antMatchers("/input", "/output").hasAnyRole("USER")
-					.anyRequest().authenticated()
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/saml*").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-					.loginPage("/login")
-					.permitAll()
-					.and()
-                .logout()
-					.permitAll();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+            .apply(saml())
+                .serviceProvider()
+                    .keyStore()
+                        .storeFilePath("saml/keystore.jks")
+                        .password(this.password)
+                        .keyname(this.keyAlias)
+                        .keyPassword(this.password)
+                        .and()
+                    .protocol("https")
+                    .hostname(String.format("%s:%s", "localhost", this.port))
+                    .basePath("/")
+                    .and()
+                .identityProvider()
+                .metadataFilePath(this.metadataUrl);
     }
 }
